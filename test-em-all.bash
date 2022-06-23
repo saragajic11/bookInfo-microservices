7952ebe1ffc2e826c815a1c8dc528d5c3bc7b833
saragajic11 <saragajic115@gmail.com>
2022-06-23 10:04:17 +0200

    Updated test-em-all.bash script

--- test-em-all.bash
+++ test-em-all.bash
@@ -4,6 +4,10 @@
 #
 : ${HOST=localhost}
 : ${PORT=8080}
+: ${BOK_ID_RAT_COM_BTN=2}
+: ${BOK_ID_NOT_FOUND=13}
+: ${BOK_ID_NO_COM_NO_BTN=114}
+: ${BOK_ID_NO_RAT_NO_BTN=214}
 
 function assertCurl() {
 
@@ -77,6 +81,53 @@ function waitForService() {
     done
 }
 
+function testCompositeCreated() {
+
+    # Expect that the Book Composite for bookId $BOK_ID_RAT_COM_BTN has been created with three ratings, three comments and three book theme nights
+    if ! assertCurl 200 "curl $AUTH -k https://$HOST:$PORT/book-composite/$BOK_ID_RAT_COM_BTN -s"
+    then
+        echo -n "FAIL"
+        return 1
+    fi
+
+    set +e
+    assertEqual "$BOK_ID_RAT_COM_BTN" $(echo $RESPONSE | jq .movieId)
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    assertEqual 3 $(echo $RESPONSE | jq ".comments | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    assertEqual 3 $(echo $RESPONSE | jq ".raatings | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+    
+    assertEqual 3 $(echo $RESPONSE | jq ".bookThemeNights | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    set -e
+}
+
+function waitForMessageProcessing() {
+    echo "Wait for messages to be processed... "
+
+    # Give background processing some time to complete...
+    sleep 5
+
+    n=0
+    until testCompositeCreated
+    do
+        n=$((n + 1))
+        if [[ $n == 40 ]]
+        then
+            echo " Give up"
+            exit 1
+        else
+            sleep 3
+            echo -n ", retry #$n "
+        fi
+    done
+    echo "All messages are now processed!"
+}
+
 function recreateComposite() {
     local bookId=$1
     local composite=$2
@@ -103,16 +154,32 @@ function setupTestdata() {
     ]}'
     recreateComposite 1 "$body"
 
-    body=\
-'{"bookId":1,"name":"Book 1","releaseDate":"2021-08-12","language":"Language 1", "ratings":[
+
+    body="{\"bookId\":$BOK_ID_NO_COM_NO_BTN"
+    body+=\
+',"name":"Book 1","releaseDate":"2021-08-12","language":"Language 1", "ratings":[
         {"ratingId":1,"author":"Author 1","rating":1},
         {"ratingId":2,"author":"Author 2","rating":2},
         {"ratingId":3,"author":"Author 3","rating":3},
-    ]}'
-    recreateComposite 113 "$body"
+]}'
+    recreateComposite "$BOK_ID_NO_COM_NO_BTN" "$body"
 
-    body=\
-'{"bookId":1,"name":"Book 1","releaseDate":"2021-08-12","language":"Language 1", "comments":[
+    body="{\"bookId\":$BOK_ID_NO_RAT_NO_BTN"
+    body+=\
+',"bookId":1,"name":"Book 1","releaseDate":"2021-08-12","language":"Language 1", "comments":[
+        {"commentId":1,"author":"Author 1","content":"content 1"},
+        {"commentId":2,"author":"Author 2","content":"content 2"},
+        {"commentId":3,"author":"Author 3","content":"content 3"}
+    ]}'
+    recreateComposite "$BOK_ID_NO_RAT_NO_BTN" "$body"
+    
+    body="{\"bookId\":$BOK_ID_RAT_COM_BTN"
+    body+=\
+',"name":"Book 1","releaseDate":"2021-08-12","language":"Language 1", "ratings":[
+        {"ratingId":1,"author":"Author 1","rating":1},
+        {"ratingId":2,"author":"Author 2","rating":2},
+        {"ratingId":3,"author":"Author 3","rating":3},
+    ], "comments":[
         {"commentId":1,"author":"Author 1","content":"content 1"},
         {"commentId":2,"author":"Author 2","content":"content 2"},
         {"commentId":3,"author":"Author 3","content":"content 3"}
@@ -121,8 +188,7 @@ function setupTestdata() {
         {"bookThemeNightId":2,"name":"name 2","startDate":"2021-08-12", "location": "Location 2"},
         {"bookThemeNightId":3,"name":"name 3","startDate":"2021-08-12", "location": "Location 3"}
     ]}'
-    recreateComposite 213 "$body"
-
+    recreateComposite "$BOK_ID_RAT_COM_BTN" "$body"
 }
 
 set -e
@@ -141,39 +207,34 @@ then
     docker-compose up -d
 fi
 
-waitForService curl -X DELETE http://$HOST:$PORT/book-composite/13
+waitForService curl -k http://$HOST:$PORT/actuator/health
 
 setupTestdata
 
+waitForMessageProcessing
+
 # Verify that a normal request works
-assertCurl 200 "curl http://$HOST:$PORT/book-composite/1 -s"
-assertEqual 1 $(echo $RESPONSE | jq .bookId)
+assertCurl 200 "curl http://$HOST:$PORT/book-composite/$BOK_ID_RAT_COM_BTN -s"
+assertEqual $BOK_ID_RAT_COM_BTN $(echo $RESPONSE | jq .bookId)
 assertEqual 3 $(echo $RESPONSE | jq ".comments | length")
 assertEqual 3 $(echo $RESPONSE | jq ".ratings | length")
 assertEqual 3 $(echo $RESPONSE | jq ".bookThemeNights | length")
 
 # Verify that a 404 (Not Found) error is returned for a non existing insuranceCompanyId 13
-assertCurl 404 "curl http://$HOST:$PORT/book-composite/13 -s"
+assertCurl 404 "curl http://$HOST:$PORT/book-composite/$BOK_ID_NOT_FOUND -s"
 
 # Verify that no comments are returned for insuranceCompanyId 113
-assertCurl 200 "curl http://$HOST:$PORT/book-composite/113 -s"
-assertEqual 113 $(echo $RESPONSE | jq .bookId)
+assertCurl 200 "curl http://$HOST:$PORT/book-composite/$BOK_ID_NO_COM_NO_BTN -s"
+assertEqual $BOK_ID_NO_COM_NO_BTN $(echo $RESPONSE | jq .bookId)
 assertEqual 0 $(echo $RESPONSE | jq ".comments | length")
 assertEqual 3 $(echo $RESPONSE | jq ".ratings | length")
-assertEqual 3 $(echo $RESPONSE | jq ".bookThemeNights | length")
+assertEqual 0 $(echo $RESPONSE | jq ".bookThemeNights | length")
 
 # Verify that no ratings are returned for insuranceCompanyId 213
-assertCurl 200 "curl http://$HOST:$PORT/book-composite/213 -s"
-assertEqual 213 $(echo $RESPONSE | jq .insuranceCompanyId)
+assertCurl 200 "curl http://$HOST:$PORT/book-composite/$BOK_ID_NO_RAT_NO_BTN -s"
+assertEqual $BOK_ID_NO_RAT_NO_BTN $(echo $RESPONSE | jq .insuranceCompanyId)
 assertEqual 3 $(echo $RESPONSE | jq ".comments | length")
 assertEqual 0 $(echo $RESPONSE | jq ".ratings | length")
-assertEqual 3 $(echo $RESPONSE | jq ".bookThemeNights | length")
-
-# Verify that no bookThemeNights are returned for insuranceCompanyId 313
-assertCurl 200 "curl http://$HOST:$PORT/book-composite/313 -s"
-assertEqual 313 $(echo $RESPONSE | jq .insuranceCompanyId)
-assertEqual 3 $(echo $RESPONSE | jq ".comments | length")
-assertEqual 3 $(echo $RESPONSE | jq ".ratings | length")
 assertEqual 0 $(echo $RESPONSE | jq ".bookThemeNights | length")
 
 # Verify that a 422 (Unprocessable Entity) error is returned for a bookId that is out of range (-1)
